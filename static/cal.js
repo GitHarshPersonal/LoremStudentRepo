6ac4458fbb9524b936e54f9d275d402b74875fe9
GitHarshSaxena <100764821+GitHarshSaxena@users.noreply.github.com>
2022-05-16 13:09:12 +0530

    Add javaScript for calculator

---
+++ static/cal.js
@@ -0,0 +1,30 @@
+//function that display value
+function dis(val)
+{
+    document.getElementById("result").value+=val
+}
+  
+//function that evaluates the digit and return result
+function solve()
+{
+    let x = document.getElementById("result").value
+    let y = eval(x)
+    document.getElementById("result").value = y
+}
+  
+//function that clear the display
+function clr()
+{
+    document.getElementById("result").value = ""
+}
+
+document.getElementById("result").addEventListener("keyup", function(event){
+    if(event.keyCode === 13 ){
+        solve();
+    }
+});
+document.getElementById("result").addEventListener("keyup", function(event){
+    if(event.keyCode === 46 ){
+        clr();
+    }
+});
